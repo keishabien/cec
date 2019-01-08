b5adf9ba0d827ba7e748c4f621f7cacc050a53a7
Keisha Bien <kbien@mdm.lcl>
2019-01-08 17:08:32 -0600

    got search function to return the correct office information

--- app/sprinkles/site/src/Controller/SearchController.php
+++ app/sprinkles/site/src/Controller/SearchController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace UserFrosting\Sprinkle\Site\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
@@ -7,15 +8,25 @@ use Slim\Exception\NotFoundException;
 use UserFrosting\Sprinkle\Core\Controller\SimpleController;
 use UserFrosting\Support\Exception\ForbiddenException;
 use UserFrosting\Sprinkle\Site\Database\Models\Search;
+use UserFrosting\Sprinkle\Site\Database\Models\Office;
 
 class SearchController extends SimpleController
 {
     public function pageSearch($request, $response, $args)
     {
+        $location = $_GET['location_name'];
 
-        $results = Search::all();
+        $results = Search::distinct()->where('office_name', 'like', '%' . $_GET['location_name'] . '%')->get();
 
-        $location = $args['location'];
+        $office = Office::distinct()->where(function ($b) {
+            $b->where('page_title', 'like', '%' . $_GET['location_name'] . '%')
+                ->orWhere('zip', 'like', '%' . $_GET['location_name'] . '%');
+        })->get();
+
+
+        $merged = $results->merge($office);
+
+
 //
         $getParams = $request->getQueryParams();
 //
@@ -28,9 +39,11 @@ class SearchController extends SimpleController
 //        }
 
         return $this->ci->view->render($response, 'pages/search.html.twig', [
-            'results'   =>  $results,
-            'location'       =>  $location,
-            'get'       =>  $_GET["submit"]
+            'results' => $results,
+            'merged'    => $merged,
+            'office'    => $office,
+            'location' => $location,
+            'get' => $_GET["submit"]
         ]);
     }
 
