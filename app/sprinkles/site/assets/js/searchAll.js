24eeae44e509ef16c4ca5f06ee3a20dae7f31910
Keisha Bien <kbien@mdm.lcl>
2019-01-10 17:05:39 -0600

    trying search stuff more

--- app/sprinkles/site/assets/js/searchAll.js
+++ app/sprinkles/site/assets/js/searchAll.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $("#searchAll").ufForm({
         msgTarget: $("#alerts-page")
     }).on("submitSuccess.ufForm", function () {
-        window.location.reload();
+        var keyword = $('.search').value();
+        window.location.replace(site.uri.public + "/search?keyword=" + keyword);
     });
 
     console.log('test');
--- app/sprinkles/site/routes/routes.php
+++ app/sprinkles/site/routes/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use UserFrosting\Sprinkle\Site\Database\Models\Search;
+
 $app->get('/', 'UserFrosting\Sprinkle\Site\Controller\PageController:pageIndex')
     ->add('authGuard')
     ->setName('index');
@@ -7,10 +9,15 @@ $app->get('/', 'UserFrosting\Sprinkle\Site\Controller\PageController:pageIndex')
 $app->get('/members', 'UserFrosting\Sprinkle\Site\Controller\PageController:pageMembers')
     ->add('authGuard');
 
-$app->get('/search/{location}', 'UserFrosting\Sprinkle\Site\Controller\SearchController:pageSearch')
+$app->get('/search', 'UserFrosting\Sprinkle\Site\Controller\SearchController:pageSearch')
     ->setName('search')
     ->add('authGuard');
 
-$app->get('/office', 'UserFrosting\Sprinkle\Site\Controller\OfficeController:pageOffice')
+//$app->get('/search/{location}', function ($request, $response, $args) {
+//
+//        echo "Hello, " . $args['location'];
+//});
+
+$app->get('/office/{page_id}/{office_name}', 'UserFrosting\Sprinkle\Site\Controller\OfficeController:pageOffice')
     ->setName('office')
     ->add('authGuard');
