7906df6b582ab93fa55b8fac8f0c7dbe75a5621c
Keisha Bien <kbien@mdm.lcl>
2019-03-21 13:21:44 -0500

    adding one js at a time

--- app/sprinkles/cec/assets/js/intake.js
+++ app/sprinkles/cec/assets/js/intake.js
@@ -1,21 +1,22 @@
-// // Collections
-// $('.repeat-div').click(function () {
-//     $(this).parent().prev().ufCollection('addRow');
-// });
-//
-// $('#dentist-div').ufCollection({
-//     useDropdown: false,
-//     rowTemplate: $('#dentist-details-row').html()
-// });
-// $('#hygienist-div').ufCollection({
-//     useDropdown: false,
-//     rowTemplate: $('#hygienist-details-row').html()
-// });
-// $('#adult-div').ufCollection({
-//     useDropdown: false,
-//     rowTemplate: $('#adult-row').html()
-// });
-// $('#child-div').ufCollection({
-//     useDropdown: false,
-//     rowTemplate: $('#child-row').html()
-// });
+// Collections
+$('.repeat-div').click(function () {
+    'use strict';
+    $(this).parent().prev().ufCollection('addRow');
+});
+
+$('#dentist-div').ufCollection({
+    useDropdown: false,
+    rowTemplate: $('#dentist-details-row').html()
+});
+$('#hygienist-div').ufCollection({
+    useDropdown: false,
+    rowTemplate: $('#hygienist-details-row').html()
+});
+$('#adult-div').ufCollection({
+    useDropdown: false,
+    rowTemplate: $('#adult-row').html()
+});
+$('#child-div').ufCollection({
+    useDropdown: false,
+    rowTemplate: $('#child-row').html()
+});
