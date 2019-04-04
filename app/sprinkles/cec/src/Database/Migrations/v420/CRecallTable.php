8688ebbbc826a0d060a4905b6c8dbf6522be0b2a
Keisha Bien <kbien@mdm.lcl>
2019-04-04 11:45:45 -0500

    updating migrations

--- app/sprinkles/cec/src/Database/Migrations/v420/CRecallTable.php
+++ app/sprinkles/cec/src/Database/Migrations/v420/CRecallTable.php
@@ -8,7 +8,8 @@ use UserFrosting\Sprinkle\Core\Database\Migration;
 class CRecallTable extends Migration
 {
     public static $dependencies = [
-        '\UserFrosting\Sprinkle\Cec\Database\Migrations\v420\OfficeTable'
+        '\UserFrosting\Sprinkle\Cec\Database\Migrations\v420\OfficeTable',
+        '\UserFrosting\Sprinkle\Cec\Database\Migrations\v420\DentistTable'
     ];
 
     public function up()
@@ -17,14 +18,18 @@ class CRecallTable extends Migration
             $this->schema->create('crecall_details', function (Blueprint $table) {
                 $table->increments('id');
                 $table->integer('office_id')->unsigned();
+                $table->integer('dentist_id')->unsigned();
                 $table->string('age_range', 255)->nullable();
                 $table->string('braces_units', 255)->nullable();
                 $table->string('dr_units', 255)->nullable();
                 $table->string('hyg_units', 255)->nullable();
+                $table->string('notes', 1000)->nullable();
                 $table->timestamps();
 
                 $table->foreign('office_id')->references('office_id')->on('office_details');
                 $table->index('office_id');
+                $table->foreign('dentist_id')->references('id')->on('dentist_details');
+                $table->index('dentist_id');
                 $table->engine = 'InnoDB';
                 $table->collation = 'utf8_unicode_ci';
                 $table->charset = 'utf8';
--- app/sprinkles/cec/src/Database/Migrations/v420/HygienistTable.php
+++ app/sprinkles/cec/src/Database/Migrations/v420/HygienistTable.php
@@ -8,7 +8,8 @@ use UserFrosting\Sprinkle\Core\Database\Migration;
 class HygienistTable extends Migration
 {
     public static $dependencies = [
-        '\UserFrosting\Sprinkle\Cec\Database\Migrations\v420\OfficeTable'
+        '\UserFrosting\Sprinkle\Cec\Database\Migrations\v420\OfficeTable',
+        '\UserFrosting\Sprinkle\Cec\Database\Migrations\v420\DentistTable'
     ];
 
     public function up()
@@ -17,6 +18,7 @@ class HygienistTable extends Migration
             $this->schema->create('hygienist_details', function (Blueprint $table) {
                 $table->increments('id');
                 $table->integer('office_id')->unsigned();
+                $table->integer('dentist_id')->unsigned();
                 $table->string('name', 255)->nullable();
                 $table->string('nickname', 255)->nullable();
                 $table->string('provider_num', 255)->nullable();
@@ -27,6 +29,8 @@ class HygienistTable extends Migration
 
                 $table->foreign('office_id')->references('office_id')->on('office_details');
                 $table->index('office_id');
+                $table->foreign('dentist_id')->references('id')->on('dentist_details');
+                $table->index('dentist_id');
                 $table->engine = 'InnoDB';
                 $table->collation = 'utf8_unicode_ci';
                 $table->charset = 'utf8';
