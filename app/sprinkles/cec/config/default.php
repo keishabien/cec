427a5d753ba65590d3d79266fc3e760addff148f
Keisha Bien <kbien@mdm.lcl>
2019-04-10 14:32:50 -0500

    smtp

--- app/sprinkles/cec/config/default.php
+++ app/sprinkles/cec/config/default.php
@@ -24,5 +24,25 @@ return [
     'php' => [
         'timezone' => 'America/Chicago',
         'log_errors' => true
+    ],
+    'debug' => [
+        'smtp' => true,
+        'queries' => true,
+        'ajax' => true
+    ],
+    'mail' => [
+        'mailer' => 'smtp', // Set to one of 'smtp', 'mail', 'qmail', 'sendmail'
+        'host' => getenv('SMTP_HOST') ?: null,
+        'port' => 587,
+        'auth' => true,
+        'secure' => 'tls',
+        'username' => getenv('SMTP_USER') ?: null,
+        'password' => getenv('SMTP_PASSWORD') ?: null,
+        'smtp_debug' => 4,
+        'message_options' => [
+            'CharSet' => 'UTF-8',
+            'isHtml' => true,
+            'Timeout' => 15
+        ]
     ]
 ];
