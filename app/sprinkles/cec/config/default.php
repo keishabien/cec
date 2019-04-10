dc56036a905eca2a83e5d3b4eafd9c80a6badbe9
Keisha Bien <kbien@mdm.lcl>
2019-04-10 15:17:34 -0500

    trying anotha one

--- app/sprinkles/cec/config/default.php
+++ app/sprinkles/cec/config/default.php
@@ -33,7 +33,7 @@ return [
     'mail' => [
         'mailer' => 'smtp', // Set to one of 'smtp', 'mail', 'qmail', 'sendmail'
         'host' => getenv('SMTP_HOST') ?: null,
-        'port' => 465,
+        'port' => 25,
         'auth' => false,
         'secure' => false,
         'username' => getenv('SMTP_USER') ?: null,
