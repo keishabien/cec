1d2d3d755696cc7076feb77e1a03de74a05100e2
Keisha Bien <kbien@mdm.lcl>
2019-04-10 12:39:12 -0500

    remove required message for now

--- app/sprinkles/cec/src/Controller/PageController.php
+++ app/sprinkles/cec/src/Controller/PageController.php
@@ -18,9 +18,9 @@ class PageController extends SimpleController
 {
     public function pageIndex($request, $response, $args)
     {
-        $ms = $this->ci->alerts;
-
-        $ms->addMessageTranslated('success', 'ACCOUNT.REQUIRED');
+//        $ms = $this->ci->alerts;
+//
+//        $ms->addMessageTranslated('success', 'ACCOUNT.REQUIRED');
         return $this->ci->view->render($response, 'pages/index.html.twig');
     }
 
